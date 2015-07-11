c7aab6b1ce10bfab5263707b5fdc809f7da240c3
Induction <jaskarans-optimusinfo.com>
2015-07-11 13:09:59 +0530

    DAY 4(ASSIGNMENT 2)

--- sql_induction/SQLAssignment2.sql
+++ sql_induction/SQLAssignment2.sql
@@ -11,6 +11,8 @@ Emp_DOJ DATE NOT NULL
 INSERT INTO t_emp VALUES(1001,'OPT20110105','Manmohan',' ','Singh','19830210','20100525');
 
 INSERT INTO t_emp VALUES(1003,'OPT20100915','ALFRED',' Joseph','Lawrence','19880228','20110524');
+INSERT INTO t_emp VALUES(1005,'OPT20100917','Ammy',' Joseph','Smith','19880131','20110524');
+
 SELECT * FROM t_emp;
 CREATE TABLE t_activity(
 Activity_id INT PRIMARY KEY,
@@ -49,19 +51,46 @@ INSERT INTO t_salary VALUES(1001,1003,'20110216',20000);
 INSERT INTO t_salary VALUES(1002,1003,'20110105',25000);
 INSERT INTO t_salary VALUES(1003,1001,'20110216',26000);
 
+-- Q1. DISPLAY FULL NAME AND DATE OF BIRTH  THOSE EMPLOYEES WHOSE BIRTH DATE FALLS IN THE LAST DAY OF ANY MONTH
 SELECT Emp_f_name+Emp_m_name+Emp_l_name AS FullName,Emp_DOB 
 FROM t_emp 
-WHERE Emp_DOB IN(
-SELECT
- 
-CASE 
-	 WHEN EOMONTH(Emp_DOB)=Emp_DOB
-	THEN Emp_DOB 
-	ELSE 
-	NULL 
-	END 
-	FROM t_emp ); 
+WHERE DATEDIFF(day,Emp_DOB,EOMONTH (Emp_DOB))=0;
 
-SELECT EOMONTH ( '12/1/2011' ) AS Result;
 
-	
+
+
+--SELECT * FROM t_atten_det;
+--SELECT (SELECT Emp_f_name+' '+Emp_m_name+' '+Emp_L_name FROM t_emp WHERE s.Emp_id=t_emp.Emp_Id ) AS Full_Name
+--,(SELECT MAX(t_salary.New_Salary) FROM t_salary WHERE t_salary.New_Salary NOT IN (SELECT MAX(t_salary.New_Salary) FROM t_salary)) AS Previous_Salary
+--,MAX(s.New_Salary) AS Current_Salary,SUM(CASE WHEN e.Emp_Id=a.Emp_id THEN A.Atten_end_hrs ELSE 0 END) AS Total_Worked_Hours,MAX(a.Atten_start_datetime)
+--FROM (t_emp e INNER JOIN t_atten_det a ON  e.Emp_Id= a.Emp_id) INNER JOIN t_salary S ON s.Emp_id=e.Emp_id 
+--GROUP BY s.Emp_id,e.Emp_Id,a.Emp_id;
+
+
+--SELECT (SELECT t_emp.Emp_f_name+' '+t_emp.Emp_m_name+' '+t_emp.Emp_L_name FROM t_emp  WHERE s.Emp_id=t_emp.Emp_Id ) AS Full_Name
+
+--FROM (t_emp e INNER JOIN t_salary s ON  e.Emp_Id= s.Emp_id) INNER JOIN t_atten_det a ON a.Emp_id=s.Emp_id
+--GROUP BY e.Emp_id;
+
+
+
+--SELECT E.EMP_ID,MAX(e.Emp_f_name+' '+e.Emp_m_name+' '+e.Emp_L_name ) AS Full_Name,MAX(s.New_Salary) AS Current_Salary,(SELECT MAX(t_salary.New_Salary) FROM t_salary WHERE t_salary.New_Salary NOT IN (SELECT MAX(t_salary.New_Salary) FROM t_salary)) AS Previous_Salary
+--,(MAX(s.New_Salary) -(SELECT MAX(t_salary.New_Salary) FROM t_salary WHERE t_salary.New_Salary NOT IN (SELECT MAX(t_salary.New_Salary) FROM t_salary))) AS Increment
+--FROM (t_emp e INNER JOIN t_salary s ON  e.Emp_Id= s.Emp_id) 
+--GROUP BY E.Emp_id;
+
+--SELECT emp.Emp_Id,SUM(Atten_end_hrs) AS Total_Worked_Hours ,(SELECT t_activity.Activity_description FROM t_atten_det INNER JOIN t_activity ON t_atten_det.Activity_id=t_activity.Activity_id WHERE Atten_start_datetime=MAX(ATT.Atten_start_datetime)) AS Last_Worked_Activity,(SELECT Atten_end_hrs FROM t_atten_det WHERE t_atten_det.Atten_start_datetime=MAX(ATT.Atten_start_datetime)) AS Hours_Worked_In_Last_Activity
+--FROM (t_atten_det att INNER JOIN t_emp emp ON att.Emp_id=emp.Emp_Id)
+--GROUP BY emp.Emp_Id;
+
+--Q2. DISPLAY EMPLOYEE FULL NAME , GOT INCREMENT IN SALARY?,PREVIOUS SALARY,CURRENT SALARY,TOTAL WORKED HOURS,HOURS WORKED 
+
+Select A.Emp_id,A.Full_Name,A.Increment,A.Previous_Salary,A.Current_Salary,B.Total_Worked_Hours,B.Last_Worked_Activity,B.Hours_Worked_In_Last_Activity
+FROM (SELECT E.EMP_ID,MAX(e.Emp_f_name+' '+e.Emp_m_name+' '+e.Emp_L_name ) AS Full_Name,MAX(s.New_Salary) AS Current_Salary,(SELECT MAX(t_salary.New_Salary) FROM t_salary WHERE t_salary.New_Salary NOT IN (SELECT MAX(t_salary.New_Salary) FROM t_salary)) AS Previous_Salary
+,(MAX(s.New_Salary) -(SELECT MAX(t_salary.New_Salary) FROM t_salary WHERE t_salary.New_Salary NOT IN (SELECT MAX(t_salary.New_Salary) FROM t_salary))) AS Increment
+FROM (t_emp e INNER JOIN t_salary s ON  e.Emp_Id= s.Emp_id) 
+GROUP BY E.Emp_id)
+AS a INNER JOIN 
+(SELECT emp.Emp_Id,SUM(Atten_end_hrs) AS Total_Worked_Hours ,(SELECT t_activity.Activity_description FROM t_atten_det INNER JOIN t_activity ON t_atten_det.Activity_id=t_activity.Activity_id WHERE Atten_start_datetime=MAX(ATT.Atten_start_datetime)) AS Last_Worked_Activity,(SELECT Atten_end_hrs FROM t_atten_det WHERE t_atten_det.Atten_start_datetime=MAX(ATT.Atten_start_datetime)) AS Hours_Worked_In_Last_Activity
+FROM (t_atten_det att INNER JOIN t_emp emp ON att.Emp_id=emp.Emp_Id)
+GROUP BY emp.Emp_Id) AS b ON A.Emp_Id=B.Emp_Id;
