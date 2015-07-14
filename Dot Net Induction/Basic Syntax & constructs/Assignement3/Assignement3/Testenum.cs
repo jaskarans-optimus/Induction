b65e7f85520500857584ff4d0a5fed0b3c8fc5d4
Induction <jaskarans-optimusinfo.com>
2015-07-14 20:01:31 +0530

    day 6

---
+++ Dot Net Induction/Basic Syntax & constructs/Assignement3/Assignement3/Testenum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement3
+{
+    class Testenum
+    {
+        enum Days { Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday };
+         static void Main()
+        {
+            foreach (string str in Enum.GetNames(typeof(Days)))
+                Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
