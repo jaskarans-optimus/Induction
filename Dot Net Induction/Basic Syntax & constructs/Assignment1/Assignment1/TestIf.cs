ea8740b6f31e90809646268a1e707dd28df65e4a
Induction <jaskarans-optimusinfo.com>
2015-07-14 19:44:54 +0530

    day 6

---
+++ Dot Net Induction/Basic Syntax & constructs/Assignment1/Assignment1/TestIf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class TestIf
+    {
+        public static void Main()
+        {
+            string languageChoice = System.Console.ReadLine();
+            if (languageChoice.Equals("VB", StringComparison.InvariantCultureIgnoreCase))
+            {
+                System.Console.WriteLine("VB .NET: OOP, multithreading andmore!");
+            }
+            else if (languageChoice.Equals("C#", StringComparison.InvariantCultureIgnoreCase))
+                Console.WriteLine("Good choice, C# is a fine language");
+            else
+                Console.WriteLine("Well...good luck with that!");
+
+            Console.ReadKey();
+        }
+    }
+}
