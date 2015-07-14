35230012d5f83f3db5fc8649bd4ce5e1d4b9d58f
Induction <jaskarans-optimusinfo.com>
2015-07-14 20:01:12 +0530

    day 6

---
+++ Dot Net Induction/Basic Syntax & constructs/Assignment2/Assignment2/TestSwitch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class TestSwitch
+    {
+         static void Main()
+        {
+
+            string languageChoice = System.Console.ReadLine();
+            switch (languageChoice)
+            {
+                case "VB":
+                    Console.WriteLine("VB .NET: OOP, multithreading andmore!");
+                    break;
+                case "C#":
+                    Console.WriteLine("Good choice, C# is a fine language");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
