084711c158ef4157de3ad5fc2ba7386c4003668a
Induction <jaskarans-optimusinfo.com>
2015-07-15 17:17:26 +0530

    day 7

---
+++ Dot Net Induction/Basic Syntax & constructs/Assignment10/Assignment10/CollegeStudent.cs
@@ -0,0 +1,21 @@
+﻿//Problem 1: Mention if the code below will run successfully or not.
+namespace Assignment10
+{
+    class Student
+    {
+        protected int _age;
+        protected internal int _rollNo;
+    }
+    class CollegeStudent
+    {
+        public static void Main()
+        {
+            CollegeStudent stu = new CollegeStudent();
+            stu._age = 10;
+            //Code will fail
+            //CollegeStudent class object do not have access to the '_age' field of the 'Student' class
+            //as '_age' field's access specifier so only the class that inherits 'Student' class will have access this field
+        }
+    }
+
+}
