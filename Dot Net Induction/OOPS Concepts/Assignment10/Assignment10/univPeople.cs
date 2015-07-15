81022c2aa320f08ee2d635b433445c3f68693aba
Induction <jaskarans-optimusinfo.com>
2015-07-15 17:22:25 +0530

    day 7

---
+++ Dot Net Induction/OOPS Concepts/Assignment10/Assignment10/univPeople.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Assignment10
+{
+    class univPeople:Student
+    {
+        Students[] students = new Student[3];
+        WorkingStaff[] staffMembers;
+        public univPeople(){
+            Student Ankit = new Student();
+            Ankit.fullName="Ankit Mathur";
+            Ankit._age = 25;
+            //As '_age'  field is declared internal ,we cannot set the value of '_age' because we need to be in the same assembly as Student to access _age
+           
+            students[0]=Ankit;
+    }
+}
