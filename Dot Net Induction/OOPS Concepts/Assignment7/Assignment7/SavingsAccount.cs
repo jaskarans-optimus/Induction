db38b477f540cdbb25ad647f043c9b038969b75e
Induction <jaskarans-optimusinfo.com>
2015-07-15 08:48:52 +0530

    day 6

---
+++ Dot Net Induction/OOPS Concepts/Assignment7/Assignment7/SavingsAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Assignment7
+{
+  
+    
+        class SavingsAccount
+        {
+            public double currBalance;
+            public static double currInterestRate = 0.04;
+            public SavingsAccount(double balance)
+            {
+                currBalance = balance;
+            }
+            public static void SetInterestRate(double newRate)
+            {
+                currInterestRate = newRate;
+            }
+            public static double GetInterestRate()
+            {
+                return currInterestRate;
+            }
+            public void SetInterestRateObj(double newRate)
+            {
+                currInterestRate = newRate;
+            }
+            public double GetInterestRateObj() { return currInterestRate; }
+            static void Main(string[] args)
+            {
+                SavingsAccount s1 = new SavingsAccount(50);
+                // An object s1 is created whose  'currBalance' field  is set to 50
+
+                SavingsAccount s2 = new SavingsAccount(100);
+                // An object s1 is create whose 'currBalace' field is set to 100
+
+                Console.WriteLine("Interest Rate is: {0}", s1.GetInterestRateObj());
+                //s1.getInterestRateObj() returns the default value of currInterestRate=0.04
+
+                s2.SetInterestRateObj(0.08);
+                // s2.SetInterestRateObj sets the value of currInterestRate . 
+                //Since 'currInterestRate' is static , all objects will share the value 0.08
+                Console.WriteLine("Interest Rate is: {0}", s1.GetInterestRateObj());
+                //this will give output 0.08 as explained above
+
+                SavingsAccount s3 = new SavingsAccount(10000.75);
+                Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+                // this will also give '0.08'  as Output
+            }
+        }
+    }
+    /*
+     * Output 
+     * Interest Rate is: 0.04
+     * Interest Rate is: 0.08
+     * Interest Rate is: 0.08
+
+    */
+
---
+++ Dot Net Induction/OOPS Concepts/Assignment8/Assignment8/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+namespace Assignment8
+{
+    
+    class Student
+    {
+
+        enum StudentAttribute { name, erollNumber,fathersName, marks, collegeName, semester };
+        String name;
+        long enrollNumber;
+
+        String fathersName;
+        int[] marks;
+        String collegeName;
+        int semester;
+        public Student()
+        {
+
+        }
+        public Student(String name,long enrollNumber,String fathersName,int[] marks,String collegeName,int semester)
+        {
+            this.name = name;
+            this.enrollNumber = enrollNumber;
+         
+            this.fathersName = fathersName;
+            this.marks = marks;
+            this.collegeName = collegeName;
+            this.semester = semester;
+        }
+        public String Name { 
+            get
+            {
+                return name;
+            }
+            set 
+            {
+                name = value;
+            }
+        }
+        public long EnrollNumber 
+        {
+            get
+            {
+                return enrollNumber;
+            }
+          
+        }
+    
+        public String  FathersName 
+        {
+            get
+            {
+                return fathersName;
+            }
+            set
+            {
+                fathersName = value;
+            }
+        }
+        public String CollegeName 
+        {
+            get 
+            {
+                return collegeName;
+            }
+            set
+            {
+                collegeName = value;
+            }
+        }
+        public int Semester 
+        {
+            get
+            {
+                return semester;
+            }
+            set
+            {
+                semester = value;
+            }
+        }
+        public int[] GetMarks()
+        {
+            return marks;
+        }
+        public void SetMarks(int[] marks)
+        {
+            this.marks = marks;
+        }
+        public void printMarks(int[] marks)
+        {
+            for (int i = 0; i < marks.Length; i++)
+                Console.WriteLine(marks[i]);
+        }
+        public double CalculatePercentage()
+        {
+            double percentage=0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                percentage += marks[i];
+            }
+            return (percentage / marks.Length)*100;
+        }
+        public void PrintAllDetails()
+        {
+            Console.WriteLine("----------------WELCOME----------------");
+            Console.WriteLine("Name:"+Name);
+            Console.WriteLine("Enrollment Number:" + EnrollNumber);
+            Console.WriteLine("Father's Name:" + fathersName);
+            Console.WriteLine("Marks:"); printMarks(marks);
+            Console.WriteLine("College Name:" + collegeName);
+            Console.WriteLine("Semester:" + semester);
+            
+            
+        }
+
+        public void print(int choice)
+        {
+            
+            switch (choice)
+            { 
+                case 0:
+                    Console.Write("Name:" + name);
+                    break;
+                case 1:
+                    Console.WriteLine("Enrollment Number:" + enrollNumber);break;
+                case 2:
+                    Console.WriteLine("Father's Name:" + fathersName);break;
+                case 3: 
+                    Console.WriteLine("Marks:"); printMarks(marks);break;
+                case 4:
+                    Console.WriteLine("College Name:" + collegeName);break;
+                case 5:
+                   Console.WriteLine("Semester:" + semester);break;
+                default:
+                    Console.WriteLine("Choice invalid");break;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            
+            Student student = new Student("Karan",random.Next(1,Int32.MaxValue),"Manno",new int[]{76,65,58,70,69},"ABCInstitute",6);
+            student.PrintAllDetails();
+            student.print(3);
+            Console.ReadKey();
+
+        }
+    }
+}
