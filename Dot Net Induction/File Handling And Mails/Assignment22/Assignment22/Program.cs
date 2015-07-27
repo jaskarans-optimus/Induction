eb0262834606a33dfc966c4d9d9807b6ad541dc1
Induction <jaskarans-optimusinfo.com>
2015-07-27 18:45:46 +0530

    Assignment 22(BinaryReader and BinaryWriter)

---
+++ Dot Net Induction/File Handling And Mails/Assignment22/Assignment22/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Assignment22
+{
+    /// <summary>
+    /// This Program is used to demonstrate the use of binary Reader and Binary Writer
+    /// </summary>
+    class Program
+    {
+        const string fileName = "AppSettings.dat";
+        static void Main(string[] args)
+        {
+            try
+            {
+                WriteSomeValue();
+                DisplayValues();
+            }
+            catch (IOException ioexception)
+            {
+                Console.WriteLine("IOException:" + ioexception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Exception:" + exception.Message);
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Write Some value in the binary file
+        /// </summary>
+        public static void WriteSomeValue()
+        {
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fileName, FileMode.Create)))
+            {
+                writer.Write(250.32F);
+                writer.Write(@"C:\Users");
+                writer.Write(10);
+                writer.Write(true);
+            }
+        }
+        public static void DisplayValues()
+        {
+            float decimalValue;
+            string userDirectory;
+            int intValue;
+            bool boolValue;
+            if (File.Exists(fileName))
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+                {
+                    decimalValue = reader.ReadSingle();
+                    userDirectory = reader.ReadString();
+                    intValue = reader.ReadInt32();
+                    boolValue = reader.ReadBoolean();
+                }
+                Console.WriteLine("Decimal Value:  " +decimalValue);
+                Console.WriteLine("User Directory: "+userDirectory);
+                Console.WriteLine("Int value:      "+intValue);
+                Console.WriteLine("Bool Value:     "+boolValue);
+            }
+        }
+    }
+}
