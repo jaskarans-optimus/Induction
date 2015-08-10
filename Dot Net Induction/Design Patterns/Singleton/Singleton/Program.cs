58b163824ad57192c839bb33faf9fdba9122559c
Induction <jaskarans-optimusinfo.com>
2015-08-10 08:56:17 +0530

    Assignment (Design Patterns)

---
+++ Dot Net Induction/Design Patterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main()
+        {
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance");
+            }
+
+        }
+    }
+}
